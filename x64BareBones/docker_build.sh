f9ce3ec144ad6eb333f1ff5628499f7e06ac43f7
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-10-19 22:29:57 -0300

    docker_build con pwd

--- x64BareBones/docker_build.sh
+++ x64BareBones/docker_build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker run -d -v "$(pwd)"/x64BareBones:/root --security-opt seccomp:unconfined -ti --name ARES agodio/itba-so:1.0
+docker run -d -v "$(pwd)":/root --security-opt seccomp:unconfined -ti --name ARES agodio/itba-so:1.0
+
