9311bae0b28926510a33dba05706aeb5f73e3496
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-10-19 22:24:39 -0300

    docker_build con pwd

--- x64BareBones/docker_build.sh
+++ x64BareBones/docker_build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d -v /Users/juaniraggio/workspace/itbaworkspace/Ares/x64BareBones:/root --security-opt seccomp:unconfined -ti --name ARES agodio/itba-so:1.0
+docker run -d -v "$(pwd)"/x64BareBones:/root --security-opt seccomp:unconfined -ti --name ARES agodio/itba-so:1.0
