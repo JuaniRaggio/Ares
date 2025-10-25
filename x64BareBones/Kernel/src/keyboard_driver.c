c435be238419d6e08e7e4294d9729e30cb813964
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-10-25 17:09:20 -0300

    (cocinados)

--- x64BareBones/Kernel/src/keyboard_driver.c
+++ x64BareBones/Kernel/src/keyboard_driver.c
@@ -1,48 +1,46 @@
 #include <keyboard_driver.h>
+#include <stdint.h>
 
 typedef struct {
-    uint8_t buffer[256];
-    uint8_t read_pos;
-    uint8_t write_pos;
-    uint8_t modifiers; // Shift, Ctrl, Alt
+        uint8_t buffer[256];
+        uint8_t read_pos;
+        uint8_t write_pos;
+        uint8_t modifiers; // Shift, Ctrl, Alt
 } keyboard_state_t;
 
-keyboard_state_t keyboard = {0};
+static keyboard_state_t keyboard = {0};
 
 static uint8_t shift_pressed = 0;
 
-char keyboard_handler() {
-    uint8_t scan_code = get_input();
-    
-    // Si es un break code (tecla soltada)
-    if (scan_code & 0x80) {
-        uint8_t make_code = scan_code & 0x7F; // le saco el bit 7
+#define LSHIFT_CODE 0x2A
+#define RSHIFT_CODE 0x36
+#define BREAK_CODE 0x80
 
-        if (make_code == 0x2A || make_code == 0x36) {
-            shift_pressed = (uint8_t) 0; // se soltó Shift
+char keyboard_handler() {
+        uint8_t scan_code = get_input();
+
+        // BREAK_CODE -> Se suelta tecla
+        if (scan_code & BREAK_CODE) {
+                uint8_t make_code = scan_code & 0x7F; // le saco el bit 7
+
+                if (make_code == LSHIFT_CODE || make_code == RSHIFT_CODE) {
+                        keyboard.modifiers = (uint8_t)off;
+                }
+
+                return 0; // no imprimimos nada al soltar teclas
+        } else if (scan_code == LSHIFT_CODE || scan_code == RSHIFT_CODE) {
+                keyboard.modifiers = (uint8_t)shift; // se presionó Shift
+                return 0;
         }
 
-        return 0; // no imprimimos nada al soltar teclas
-    }
+        char c;
 
-    // Si es un make code (tecla presionada)
-    if (scan_code == 0x2A || scan_code == 0x36) {
-        shift_pressed = (uint8_t) 1; // se presionó Shift
-        return 0;
-    }
+        // Convertir scancode a carácter
+        if (shift_pressed) {
+                c = ascii_shift_table[scan_code];
+        } else {
+                c = ascii_table[scan_code];
+        }
 
-    char c;
-
-    // Convertir scancode a carácter
-    if(shift_pressed) {
-        c = ascii_shift_table[scan_code];
-    } else {
-        c = ascii_table[scan_code];
-    }
-    
-    return c;
-
+        return c;
 }
-
-
-
