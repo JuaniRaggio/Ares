66097c5b7bf09b45017098ee75d3512d72f60f67
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-11-02 23:29:26 -0300

    fix: syscall numbers

--- x64BareBones/Userland/UserCodeModule/include/syscall_numbers.h
+++ x64BareBones/Userland/UserCodeModule/include/syscall_numbers.h
@@ -9,8 +9,16 @@
 #define SYS_WRITE 0
 #define SYS_EXIT 1
 #define SYS_READ 2
+#define SYS_CLEAR 3
+#define SYS_GET_TICKS 4
+#define SYS_GET_RESOLUTION 5
+#define SYS_GET_REGISTER_ARRAY 6
+#define SYS_SET_FONT_SIZE 7
+#define SYS_GET_MEMORY 8
+#define SYS_DRAW_RECT 9
+#define SYS_GET_SECONDS 10
 
 // Cuando agregues una nueva syscall, incrementa este número
-#define SYS_MAX 2
+#define SYS_MAX 10
 
 #endif /* SYSCALL_NUMBERS_H */
