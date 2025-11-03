b17e479f8aec953f23f69808add147e9f284b94f
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-11-03 15:39:54 -0300

    feat: itoa & utoa implementation
    feat: itoa & utoa implementation

--- AresOS/Userland/UserCodeModule/libc/src/lib.c
+++ AresOS/Userland/UserCodeModule/libc/src/lib.c
@@ -87,3 +87,91 @@ char *strncpy(char *dest, const char *src, uint64_t n) {
                 ;
         return dest;
 }
+
+char *itoa(int value, char *str, int base) {
+        char *ptr  = str;
+        char *ptr1 = str;
+        char tmp_char;
+        int tmp_value;
+
+        // Validar base
+        if (base < 2 || base > 36) {
+                *str = '\0';
+                return str;
+        }
+
+        // Manejar valor 0
+        if (value == 0) {
+                *ptr++ = '0';
+                *ptr   = '\0';
+                return str;
+        }
+
+        // Manejar numeros negativos solo en base 10
+        int negative = 0;
+        if (value < 0 && base == 10) {
+                negative = 1;
+                value    = -value;
+        }
+
+        // Convertir el numero a string
+        while (value != 0) {
+                tmp_value = value % base;
+                *ptr++ =
+                    (tmp_value < 10) ? tmp_value + '0' : tmp_value - 10 + 'a';
+                value /= base;
+        }
+
+        if (negative) {
+                *ptr++ = '-';
+        }
+
+        *ptr-- = '\0';
+
+        while (ptr1 < ptr) {
+                tmp_char = *ptr;
+                *ptr--   = *ptr1;
+                *ptr1++  = tmp_char;
+        }
+
+        return str;
+}
+
+char *utoa(unsigned int value, char *str, int base) {
+        char *ptr  = str;
+        char *ptr1 = str;
+        char tmp_char;
+        unsigned int tmp_value;
+
+        // Validar base
+        if (base < 2 || base > 36) {
+                *str = '\0';
+                return str;
+        }
+
+        // Manejar valor 0
+        if (value == 0) {
+                *ptr++ = '0';
+                *ptr   = '\0';
+                return str;
+        }
+
+        // Convertir el numero a string (en orden inverso)
+        while (value != 0) {
+                tmp_value = value % base;
+                *ptr++ =
+                    (tmp_value < 10) ? tmp_value + '0' : tmp_value - 10 + 'a';
+                value /= base;
+        }
+
+        *ptr-- = '\0';
+
+        // Invertir el string
+        while (ptr1 < ptr) {
+                tmp_char = *ptr;
+                *ptr--   = *ptr1;
+                *ptr1++  = tmp_char;
+        }
+
+        return str;
+}
