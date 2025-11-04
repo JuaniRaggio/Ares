f806dae416f285511572a02045bd48d314e803b1
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-11-04 04:18:29 -0300

    fix: redefinition of static method + Makefile

--- AresOS/Kernel/include/arch/x86_64/idtLoader.h
+++ AresOS/Kernel/include/arch/x86_64/idtLoader.h
@@ -10,7 +10,6 @@
 //******************************************************************************
 
 #include <stdint.h>
-void setup_IDT_entry(int index, uint64_t offset);
 void load_idt();
 
 #endif // _IDTLOADER_H_
