9fb1a927b7cd3811efef09146b192f18e9559502
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-11-03 22:48:42 -0300

    feat: soy demasiado bueno en esto

--- AresOS/Userland/UserCodeModule/include/commands.h
+++ AresOS/Userland/UserCodeModule/include/commands.h
@@ -46,86 +46,98 @@ typedef struct {
         lambda_t lambda;
 } command_t;
 
-static command_t commands[QTY_COMMANDS] = {
-    {
-        .name        = "help",
-        .description = "List all available commands",
-        .lambda =
-            {
-                .execute.supplier = (void *)&help,
-                .ftype            = supplier_t,
-            },
-    },
-    {
-        .name        = "man",
-        .description = "Show manual for a specific command",
-        .lambda =
-            {
-                .execute.function = (void *)&man,
-                .ftype            = function_t,
-            },
-    },
-    {
-        .name        = "inforeg",
-        .description = "Display captured CPU registers",
-        .lambda =
-            {
-                .execute.supplier = (void *)&print_info_reg,
-                .ftype            = supplier_t,
-            },
-    },
-    {
-        .name        = "time",
-        .description = "Show system elapsed time",
-        .lambda =
-            {
-                .execute.supplier = (void *)&show_time,
-                .ftype            = supplier_t,
-            },
-    },
-    {
-        .name        = "div",
-        .description = "Integer division of two numbers",
-        .lambda =
-            {
-                .execute.bi_function = (void *)&div_cmd,
-                .ftype               = bi_function_t,
-            },
-    },
-    {
-        .name        = "clear",
-        .description = "Clear the entire screen",
-        .lambda =
-            {
-                .execute.supplier = (void *)&clear_cmd,
-                .ftype            = supplier_t,
-            },
-    },
-    {
-        .name        = "printmem",
-        .description = "Memory dump of 32 bytes from an address",
-        .lambda =
-            {
-                .execute.function = (void *)&print_mem,
-                .ftype            = function_t,
-            },
-    },
-    {
-        .name        = "history",
-        .description = "Show command history",
-        .lambda =
-            {
-                .execute.supplier = (void *)&history_cmd,
-                .ftype            = function_t,
-            },
-    },
-    {
-        .name        = "exit",
-        .description = "Exit Ares OS",
-        .lambda =
-            {
-                .execute.supplier = NULL,
-                .ftype            = supplier_t,
-            },
-    },
+static command_t help_command = {
+    .name        = "help",
+    .description = "List all available commands",
+    .lambda =
+        {
+            .execute.supplier = (void *)&help,
+            .ftype            = supplier_t,
+        },
+};
+
+static command_t man_command = {
+    .name        = "man",
+    .description = "Show manual for a specific command",
+    .lambda =
+        {
+            .execute.function = (void *)&man,
+            .ftype            = function_t,
+        },
+};
+
+static command_t inforeg_command = {
+    .name        = "inforeg",
+    .description = "Display captured CPU registers",
+    .lambda =
+        {
+            .execute.supplier = (void *)&print_info_reg,
+            .ftype            = supplier_t,
+        },
+};
+
+static command_t time_command = {
+    .name        = "time",
+    .description = "Show system elapsed time",
+    .lambda =
+        {
+            .execute.supplier = (void *)&show_time,
+            .ftype            = supplier_t,
+        },
+};
+
+static command_t div_command = {
+    .name        = "div",
+    .description = "Integer division of two numbers",
+    .lambda =
+        {
+            .execute.bi_function = (void *)&div_cmd,
+            .ftype               = bi_function_t,
+        },
+};
+
+static command_t clear_command = {
+    .name        = "clear",
+    .description = "Clear the entire screen",
+    .lambda =
+        {
+            .execute.supplier = (void *)&clear_cmd,
+            .ftype            = supplier_t,
+        },
+};
+
+static command_t print_mem_command = {
+    .name        = "printmem",
+    .description = "Memory dump of 32 bytes from an address",
+    .lambda =
+        {
+            .execute.function = (void *)&print_mem,
+            .ftype            = function_t,
+        },
+};
+
+static command_t history_command = {
+    .name        = "history",
+    .description = "Show command history",
+    .lambda =
+        {
+            .execute.supplier = (void *)&history_cmd,
+            .ftype            = function_t,
+        },
+};
+
+static command_t exit_command = {
+    .name        = "exit",
+    .description = "Exit Ares OS",
+    .lambda =
+        {
+            .execute.supplier = NULL,
+            .ftype            = supplier_t,
+        },
+};
+
+static command_t *commands[QTY_COMMANDS] = {
+    &help_command,      &man_command,     &inforeg_command,
+    &time_command,      &div_command,     &clear_command,
+    &print_mem_command, &history_command, &exit_command,
 };
