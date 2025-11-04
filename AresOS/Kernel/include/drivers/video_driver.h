085ab6f963119a6233e7b2f8d5b2d49522922786
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-11-04 04:09:19 -0300

    feat: Screen bufferization

--- AresOS/Kernel/include/drivers/video_driver.h
+++ AresOS/Kernel/include/drivers/video_driver.h
@@ -60,3 +60,7 @@ void drawCharDefault(char c, int x, int y, uint32_t color);
 void clearScreen(uint32_t color);
 void drawTestPattern(void);
 void drawRect(uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint32_t color);
+
+// Screen buffer for redraw
+void screen_buffer_add_char(char c);
+void screen_buffer_redraw(void);
