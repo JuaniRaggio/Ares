a61f825aed69c3fce14234f3e0c602e9fdfe4c2e
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-11-04 13:42:44 -0300

    fix: timer types

--- AresOS/Kernel/include/drivers/time.h
+++ AresOS/Kernel/include/drivers/time.h
@@ -1,9 +1,11 @@
 #ifndef _TIME_H_
 #define _TIME_H_
 
+#include <stdint.h>
+
 void timer_handler();
-int ticks_elapsed();
-int seconds_elapsed();
+uint64_t ticks_elapsed();
+uint64_t seconds_elapsed();
 void sleep(int seconds);
 
 #endif
