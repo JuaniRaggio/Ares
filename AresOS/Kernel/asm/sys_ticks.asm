423eb603937f26b1c86d934e948615de203c0d6e
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-11-04 14:39:23 -0300

    feat: sys_ticks

--- AresOS/Kernel/asm/sys_ticks.asm
+++ AresOS/Kernel/asm/sys_ticks.asm
@@ -1,8 +1,10 @@
 section .text
 global sys_get_ticks_wrapper
 global sys_get_seconds_wrapper
+global sys_get_time_wrapper
 extern sys_get_ticks
 extern sys_get_seconds
+extern sys_get_time
 
 sys_get_ticks_wrapper:
     push rbp
@@ -19,6 +21,17 @@ sys_get_seconds_wrapper:
     mov rbp, rsp
 
     call sys_get_seconds
+
+    mov rsp, rbp
+    pop rbp
+    ret
+
+sys_get_time_wrapper:
+    push rbp
+    mov rbp, rsp
+
+    ; RDI already contains the pointer to s_time
+    call sys_get_time
 
     mov rsp, rbp
     pop rbp
