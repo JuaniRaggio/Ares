ee4a642ff2b2c6a41cbddc8902cb22da7919b401
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-11-10 17:06:41 -0300

    fix: No warnings

--- AresOS/run.sh
+++ AresOS/run.sh
@@ -3,13 +3,15 @@
 # Seleccionar imagen a usar (preferir qcow2 si existe)
 if [ -f Image/x64BareBonesImage.qcow2 ]; then
     IMAGE=Image/x64BareBonesImage.qcow2
+    FORMAT=qcow2
 else
     IMAGE=Image/x64BareBonesImage.img
+    FORMAT=raw
 fi
 
 # Flag para debuggear
 if [ "$1" == "-d" ]; then
-    qemu-system-x86_64 -s -S -hda "$IMAGE" -m 512
+    qemu-system-x86_64 -s -S -drive file="$IMAGE",format="$FORMAT" -m 512
 else
-    qemu-system-x86_64 -hda "$IMAGE" -m 512
+    qemu-system-x86_64 -drive file="$IMAGE",format="$FORMAT" -m 512
 fi
