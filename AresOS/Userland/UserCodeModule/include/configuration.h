b7d6c81564824655156351b7cded1adcb8b0c9a8
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-11-11 22:03:42 -0300

    fix: color usage

--- AresOS/Userland/UserCodeModule/include/configuration.h
+++ AresOS/Userland/UserCodeModule/include/configuration.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <colors.h>
 #include <stdint.h>
 
 #define DEFAULT_HISTORY_S 10
@@ -8,19 +9,6 @@
 #define SCREEN_WIDTH 1024
 #define SCREEN_HEIGHT 768
 #define SCREEN_SIZE (SCREEN_WIDTH * SCREEN_HEIGHT)
-
-#define BLACK 0x0
-#define BLUE 0x1
-#define GREEN 0x2
-#define CYAN 0x3
-#define RED 0x4
-#define MAGENTA 0x5
-#define BROWN 0x6
-#define GREY 0x7
-#define WHITE 0xF
-#define BLACK_WHITE 0x0F
-#define GREEN_BLACK 0x20
-#define RED_WHITE 0x00FF0000
 
 typedef enum {
         block,
@@ -31,5 +19,5 @@ typedef enum {
 
 // Default Kernel font will be used
 // static const bmp_font_t *user_font = &font_ubuntu_mono;
-static const uint32_t default_font_color       = WHITE;
-static const uint32_t default_background_color = BLACK;
+static const uint32_t default_font_color       = WHITE; /* RGB white */
+static const uint32_t default_background_color = BLACK; /* RGB black */
