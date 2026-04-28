46e256cdb44ab6cca768e7cec28b659299991b07
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2026-04-28 01:31:51 -0300

    chore: formatting mrh

--- AresOS/Kernel/lib/multi_region_heap.c
+++ AresOS/Kernel/lib/multi_region_heap.c
@@ -1,14 +1,12 @@
 #include <multi_region_heap.h>
 
-heap_region_t heap_regions[] = {
-};
+heap_region_t heap_regions[] = {};
 
 typedef struct block_list {
-    struct block_list * next_free_block;
-    size_t size_in_bytes;
+        struct block_list *next_free_block;
+        size_t size_in_bytes;
 } block_list_t;
 
-size_t available_bytes = 0;
+size_t available_bytes        = 0;
 size_t successfull_alocations = 0;
-size_t successfull_frees = 0;
-
+size_t successfull_frees      = 0;
