a34b379c488fd8f3fcc5600e610963433d9aba9b
vpark2003 <vpark@itba.edu.ar>
2026-05-04 23:51:17 -0300

    avances en los semáforos

--- AresOS/Kernel/core/semaphores.c
+++ AresOS/Kernel/core/semaphores.c
@@ -19,6 +19,34 @@ void sem_system_init() {
 }
 
 static void dequeue_process(uint64_t sem_id){
+    if(semaphores[sem_id].head == NULL) return;
+
+    pNode_t *aux = semaphores[sem_id].head;
+    semaphores[sem_id].head = aux->next;
+
+    process_unblock(aux->pid);
+
+    // liberar nodo que acabamos de sacar
+
+    if(semaphores[sem_id].head == NULL){
+        semaphores[sem_id].tail = NULL;
+    }
+    return;
+}
+
+static void enqueue_process(uint64_t sem_id){
+    // creamos nodo
+    pNode_t *new_node = algo; 
+    if(semaphores[sem_id].tail == NULL){
+        semaphores[sem_id].head = new_node;
+    }else{
+        semaphores[sem_id].tail->next = new_node;
+    }
+    semaphores[sem_id].tail = new_node;
+    return;
+}
+
+static void free_queue(uint64_t sem_id){
 
 }
 
@@ -27,12 +55,13 @@ int64_t sem_init(uint64_t sem_id, int64_t value){
     semaphores[sem_id].value = value;
     return 0;
 }
+
 int64_t sem_post(uint64_t sem_id){
     if(sem_id >= MAX_SEM) return -1;
     cli();
     if(semaphores[sem_id].value >= 0){
         if(++(semaphores[sem_id].value) == 1){
-            dequeue(sem_id);
+            dequeue_process(sem_id);
         }
         sti();
         return 0;
@@ -43,16 +72,32 @@ int64_t sem_post(uint64_t sem_id){
 }
 int64_t sem_wait(uint64_t sem_id){
     if(sem_id >= MAX_SEM) return -1;
+
     cli();
     if(semaphores[sem_id].value >= 0){
         if(semaphores[sem_id].value == 0){
-            enqueue(sem_id);
-            
+            enqueue_process(sem_id);
+            if (process_block(process_getpid()) == -1){
+                sti();
+                return -1;
+            }
+            sti();
+            schedule();
+            cli();
         }
-
+        semaphores[sem_id].value--;
+        sti();
+        return 0;
     }else{
         sti();
         return -1;
     }
 }
-int64_t sem_close(uint64_t sem_id);
+
+int64_t sem_close(uint64_t sem_id){
+    if(sem_id >= MAX_SEM) return -1;
+
+    cli();
+    sti();
+
+}
