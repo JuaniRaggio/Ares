69ccc2935f96213338944d66e0ad85c8e8e2521e
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2026-06-12 17:35:04 -0300

    fix: non-development related files

--- AresOS/clean_in_container.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Script to clean the project inside a Docker container
-# Usage: ./clean_in_container.sh [CONTAINER_NAME]
-
-CONTAINER_NAME="${1:-ARES}"
-
-echo "Cleaning project in container: $CONTAINER_NAME"
-exec "$(dirname "$0")/compile_in_container.sh" "$CONTAINER_NAME" clean
